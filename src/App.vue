cf07a72cba8a471bf9608859eef2bab2b98d8737
wurong715@163.com <wurong715@163.com>
2022-09-05 22:30:06 +0800

    增加初始页面loading页面

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,16 @@
     <router-view></router-view>
   </div>
 </template>
+<script>
+  export default {
+    mounted() {
+      // 取消开始的loading动画
+      const preLoader = document.querySelector('#pre-loader')
+      preLoader.style.display = 'none'
+      document.body.ondrop = event => {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+    }
+  }
+  </script>
