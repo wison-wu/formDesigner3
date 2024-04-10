f8d81dcdf2e0e4b35084fd6d5f45f019ff2f1e0c
ChenYuMac <250187715@qq.com>
2024-04-10 21:14:50 +0800

    feat：依赖组件

--- packages/form-designer-example-app/src/main.ts
+++ packages/form-designer-example-app/src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import "form-designer-core/dist/style.css";
+import plugins from "form-designer-core";
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+//挂载相关的插件
+app.use(plugins);
 
 app.mount('#app')
