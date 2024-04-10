47e2585c315cdc28a864af41470b0e219c94b5c8
ChenYuMac <250187715@qq.com>
2024-04-10 21:14:34 +0800

    chore : 添加编译组件的环境

--- packages/form-designer-core/vite.config.ts
+++ packages/form-designer-core/vite.config.ts
@@ -13,6 +13,28 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
+  build: {
+    rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ['vue',"@vueup/vue-quill","vue3-barcode","vuedraggable","codemirror-editor-vue3","china-area-data","element-plus"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+          'vuedraggable': 'draggable',
+          'codemirror-editor-vue3': 'codeMirror',
+          '@vueup/vue-quill': 'vueQuill',
+          'vue3-barcode': 'fancyBarCode',
+          "element-plus":"elementPlus",
+        },
+      },
+    },
+    cssCodeSplit: false,
+    lib: {
+      entry: './src/components/index.ts',
+      name: 'form-designer',
+    },
+  },
 })
