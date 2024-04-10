6bf1d1ab5596a9338c08ff35e10bcc1a9e733c76
ChenYuMac <250187715@qq.com>
2024-04-10 21:32:50 +0800

    fix：修复编辑器报错

--- packages/form-designer-example-app/env.d.ts
+++ packages/form-designer-example-app/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module "*.mjs"
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
--- packages/form-designer-example-app/src/main.ts
+++ packages/form-designer-example-app/src/main.ts
@@ -7,11 +7,19 @@ import App from './App.vue'
 import router from './router'
 import "form-designer-core/dist/style.css";
 import plugins from "form-designer-core";
+// 导入element-plus
+import ElementPlus from 'element-plus';
+import ZH_CN from 'element-plus/dist/locale/zh-cn.mjs';
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 //挂载相关的插件
 app.use(plugins);
+
+// 挂载饿了么plus
+app.use(ElementPlus, {
+  locale: ZH_CN
+})
 
 app.mount('#app')
