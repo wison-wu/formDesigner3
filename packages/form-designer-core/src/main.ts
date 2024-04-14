da613e3b8d6cb3527f393359d48e5d127b1be740
ChenYuMac <250187715@qq.com>
2024-04-14 21:02:31 +0800

    refactor :：删除不需要的依赖

--- packages/form-designer-core/src/main.ts
+++ packages/form-designer-core/src/main.ts
@@ -1,4 +1,3 @@
-import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // 引入 main 组件
@@ -7,12 +6,10 @@ import App from './App.vue'
 import router from './router'
 // 导入element-plus
 import ElementPlus from 'element-plus';
+// 导入中文
 import ZH_CN from 'element-plus/dist/locale/zh-cn.mjs';
 //挂载相关的插件
 import plugins from '@/components';
-//@ts-ignore
-import { GlobalCmComponent } from "codemirror-editor-vue3";
-
 // 请求类
 import Axios from 'axios';
 // 创建app
@@ -23,8 +20,6 @@ app.use(createPinia())
 app.use(router)
 //挂载相关的插件
 app.use(plugins);
-// 挂载富文本编辑器
-app.use(GlobalCmComponent);
 // 挂载饿了么plus
 app.use(ElementPlus, {
     locale: ZH_CN
