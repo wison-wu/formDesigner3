4c763db90464d5916b4effc6125865c831fe7210
Chen Yu <250187715@qq.com>
2024-05-27 16:02:45 +0800

    revert :  重构条码为ts

--- packages/form-designer-core/src/components/custom/configs/barCode.vue
+++ packages/form-designer-core/src/components/custom/configs/barCode.vue
@@ -5,10 +5,10 @@
     </el-form-item> -->
     <el-form-item label="ID">
       <el-tooltip
-        class="item"
-        effect="dark"
-        content="请注意,ID的修改可能会导致该组件相关事件失效！"
-        placement="left"
+          class="item"
+          effect="dark"
+          content="请注意,ID的修改可能会导致该组件相关事件失效！"
+          placement="left"
       >
         <el-input class="input" v-model="props.id"></el-input>
       </el-tooltip>
@@ -26,10 +26,10 @@
       <el-input v-model="props.modelValue"></el-input>
     </el-form-item>
     <el-form-item label="条码颜色" v-show="props.lineColor">
-      <el-color-picker v-model="props.lineColor" @change="handlerChangeBarCode" />
+      <el-color-picker v-model="props.lineColor" @change="handlerChangeBarCode"/>
     </el-form-item>
     <el-form-item label="背景颜色" v-show="props.background">
-      <el-color-picker v-model="props.background" @change="handlerChangeBackground" />
+      <el-color-picker v-model="props.background" @change="handlerChangeBackground"/>
     </el-form-item>
     <!--暂不启用-->
     <!-- <el-form-item label="条码宽度">
@@ -43,31 +43,93 @@
     </el-form-item>
   </div>
 </template>
-<script>
+<script lang="ts">
+import {Component, Prop} from "web-decorator-vue";
+
 /**
- * input的配置项
+ * 条码
+ * @author chen yu
+ * @version 1.0.0
  */
-export default {
-  name: 'buttonConfig',
-  props: {
-    props: {}
-  },
-  components: {},
-  data() {
-    return {}
-  },
-  methods: {
-    handlerChangeBarCode(val) {
-      if (val == null) {
-        this.props.lineColor = '#000'
-      }
-    },
-    handlerChangeBackground(val) {
-      if (val == null) {
-        this.props.background = '#fff'
-      }
-    }
-  },
-  mounted() {}
+@Component()
+export default class BarCode {
+  // TODO 补齐作用
+  /**
+   * 参数
+   */
+  @Prop() props!: BarCodeOption;
+
+  // TODO 补齐作用
+  /**
+   *
+   * @param val
+   */
+  handlerChangeBarCode(val: any) {
+    if (val == null) this.props.lineColor = '#000'
+  }
+
+  // TODO 补齐作用
+  /**
+   *
+   * @param val
+   */
+  handlerChangeBackground(val: any) {
+    if (val == null) this.props.background = '#fff'
+  }
+}
+
+/**
+ * BarCode 的 Prop的参数
+ * @version 1.0.0
+ * @author chen yu
+ */
+export interface BarCodeOption {
+  /**
+   * id
+   * @version 1.0.0
+   */
+  id: string;
+  /**
+   * 是否显示文本
+   * @version 1.0.0
+   */
+  showLabel: boolean;
+  /**
+   * 宽度
+   * @version 1.0.0
+   */
+  labelWidth: number;
+  /**
+   * 是否显示span
+   */
+  span:number;
+  /**
+   * 条码值
+   * @version 1.0.0
+   */
+  modelValue:string;
+  /**
+   * 文本
+   * @version 1.0.0
+   */
+  label: string;
+  /**
+   * 边线颜色
+   * @version 1.0.0
+   */
+  lineColor: string;
+  /**
+   * 背景色
+   * @version 1.0.0
+   */
+  background: string;
+  /**
+   * 条码高度
+   */
+  height: number;
+  /**
+   * 显示值
+   */
+  displayValue: number;
 }
 </script>
