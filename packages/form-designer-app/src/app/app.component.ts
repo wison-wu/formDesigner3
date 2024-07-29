5973ff167d395f162b30f1f7344e4e714165965a
Chen Yu <250187715@qq.com>
2024-07-29 23:20:05 +0800

    init ng-app

---
+++ packages/form-designer-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'form-designer-app';
+}
