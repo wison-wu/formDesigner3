7db2e6557f925a63d12f1e99b309b809a709c0c0
Chen Yu <250187715@qq.com>
2024-07-29 23:05:22 +0800

    init

--- packages/form-designer-core/src/app/app.component.ts
+++ packages/form-designer-core/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
