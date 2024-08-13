ba86d17c8a32d2c7ae6f344d6bf7ebe40c3497c9
Chen Yu <250187715@qq.com>
2024-08-13 23:34:14 +0800

    refactor : 调整变量名，并且优化注释。

--- packages/form-designer-app/src/components/form-designer-core/form-designer-core.component.ts
+++ packages/form-designer-app/src/components/form-designer-core/form-designer-core.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { NgClass } from "@angular/common";
+import {Component, ViewEncapsulation} from '@angular/core';
+import {NgClass} from "@angular/common";
 import {HeaderComponent} from "../header/header.component";
 
 @Component({
@@ -14,48 +14,55 @@ import {HeaderComponent} from "../header/header.component";
   encapsulation: ViewEncapsulation.None
 })
 export class FormDesignerCoreComponent {
-  //TODO 组件显示序号
-  listClick:number = 0
-  //TODO 组件栏数据配置
-  compositeList:any = [
+  /**
+   * 组件显示序号
+   */
+  listClick: number = 0;
+  /**
+   * 组件栏数据配置
+   */
+  compositeList: any = [
     {
-      name:"组件",
-      img:"./static/zujian.svg",
-      type:"compositeList",
-      chides:[
-        {"name":"输入控件",
-          chides:[
-            {"name":"输入框","Image":""},
-            {"name":"多行输入框","Image":""},
-            {"name":"密码输入","Image":""},
-            {"name":"数字输入","Image":""},
-            {"name":"评分器","Image":""},
+      name: "组件",
+      img: "./static/zujian.svg",
+      type: "compositeList",
+      chides: [
+        {
+          "name": "输入控件",
+          chides: [
+            {"name": "输入框", "Image": ""},
+            {"name": "多行输入框", "Image": ""},
+            {"name": "密码输入", "Image": ""},
+            {"name": "数字输入", "Image": ""},
+            {"name": "评分器", "Image": ""},
           ]
         },
         {
-          "name":"布局组件",
-          chides:[
-            {"name":"输入框","Image":""},
-            {"name":"多行输入框","Image":""},
-            {"name":"密码输入","Image":""},
-            {"name":"数字输入","Image":""},
-            {"name":"评分器","Image":""},
+          "name": "布局组件",
+          chides: [
+            {"name": "输入框", "Image": ""},
+            {"name": "多行输入框", "Image": ""},
+            {"name": "密码输入", "Image": ""},
+            {"name": "数字输入", "Image": ""},
+            {"name": "评分器", "Image": ""},
           ]
         }
 
       ]
     },
     {
-      name:"大纲树",
-      type:"domList",
-      img:"./static/list-outline.svg"
+      name: "大纲树",
+      type: "domList",
+      img: "./static/list-outline.svg"
     },
     {
-      name:"历史记录",
+      name: "历史记录",
       type: "timeList",
-      img:"./static/history.svg"
+      img: "./static/history.svg"
     }
-  ]
-  //TODO 组件栏是否浮动
-  isFiexd:boolean = false
+  ];
+  /**
+   * 组件栏是否浮动
+   */
+  isFixed: boolean = false;
 }
